8cd16f23270246b45805fc1d3ac281916c4029a0
Corentin Busi <corentinbusi@gmail.com>
2025-10-30 12:27:37 +0100

    Ajout bouttons filtres + amélioration code précédent

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,20 +1,85 @@
+/* DATA RETRIEVAL AND RUN */
 
-const response = await fetch("http://localhost:5678/api/works");
-const data = await response.json();
-console.log(data);
+let data = [];
 
-for (let i = 0; i < data.length; i++) {
+async function run() {
+  const works = await fetch("http://localhost:5678/api/works");
+  data = await works.json();
+  console.log("All works : ", data);
+  genererWorks();
+  activeFilters();
+}
 
-    const sectionGallery = document.querySelector(".gallery");
+/* GENERATE ALL WORKS */
+
+const sectionGallery = document.querySelector(".gallery");
+
+function genererWorks(arrayWorks = data) {
+  sectionGallery.innerHTML = "";
+
+  for (let i = 0; i < arrayWorks.length; i++) {
     const article = document.createElement("article");
     sectionGallery.appendChild(article);
 
     const imgProject = document.createElement("img");
-    imgProject.src = data[i].imageUrl;
+    imgProject.src = arrayWorks[i].imageUrl;
+    imgProject.alt = "Projet";
     article.appendChild(imgProject);
 
     const titleProject = document.createElement("h3");
-    titleProject.innerText = data[i].title;
+    titleProject.innerText = arrayWorks[i].title;
     article.appendChild(titleProject);
     titleProject.style.padding = "1em 0em";
+  }
 }
+
+/* FILTERS */
+
+const filters = document.querySelector(".filters");
+
+function activeFilters() {
+
+  const withoutFilter = document.createElement("button");
+  withoutFilter.textContent = "Tous";
+  filters.appendChild(withoutFilter);
+  withoutFilter.addEventListener("click", () => {
+    genererWorks();
+    activeButton(withoutFilter);
+  });
+
+  const objectFilter = document.createElement("button");
+  objectFilter.textContent = "Objets";
+  filters.appendChild(objectFilter);
+  objectFilter.addEventListener("click", () => {
+    const filtered = data.filter((work) => work.categoryId === 1);
+    genererWorks(filtered);
+    activeButton(objectFilter);
+  });
+
+  const appartmentsFilter = document.createElement("button");
+  appartmentsFilter.textContent = "Appartements";
+  filters.appendChild(appartmentsFilter);
+  appartmentsFilter.addEventListener("click", () => {
+    const filtered = data.filter((work) => work.categoryId === 2);
+    genererWorks(filtered);
+    activeButton(appartmentsFilter);
+  });
+
+  const hotelsFilter = document.createElement("button");
+  hotelsFilter.textContent = "Hôtels & Restaurants";
+  filters.appendChild(hotelsFilter);
+  hotelsFilter.addEventListener("click", () => {
+    const filtered = data.filter((work) => work.categoryId === 3);
+    genererWorks(filtered);
+    activeButton(hotelsFilter);
+  });
+
+  const allbuttons = [withoutFilter, objectFilter, appartmentsFilter, hotelsFilter];
+  function activeButton(newButton) {
+    allbuttons.forEach((previousButton) => previousButton.classList.remove("active"));
+    newButton.classList.add("active");
+  };
+  activeButton(withoutFilter);
+};
+
+run();
